f213d3bd5739a27ac7ce44c641d779b11a04709e
Person0912 <kayla.jin25@ycdsbk12.ca>
2024-06-03 18:51:36 -0400

    added a functioning controls menu and exit button to the start screen

--- Sketch.java
+++ Sketch.java
@@ -3,20 +3,33 @@ import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-  // Determines what is drawn on the screen
-  int intArea = -1;
+  // Determines which area is drawn on the screen
+  int intDraw = -1;
 
-  // Start screen and control screen images
+  // Start screen
   PImage startScreen;
+
+  // Buttons on start screen
+  PImage playButton;
+  PImage controlsButton;
+  PImage exitButton;
+
+  // Controls screen
   PImage controlScreen;
 
   // Flashlight
 	PImage flashlight;
   
-  // Alex Idle Tileset
-  PImage alexIdleTileset;
+  // Alex's size readjustment
+  int intSizeProportion = 275;
 
-  // Alex Run Images
+  // Alex's Idle Images
+  PImage alexIdleBack;
+  PImage alexIdleForward;
+  PImage alexIdleLeft;
+  PImage alexIdleRight;
+
+  // Alex's Run Images
   PImage alexBack1;
   PImage alexBack2;
   PImage alexForward1;
@@ -26,12 +39,12 @@ public class Sketch extends PApplet {
   PImage alexRight1;
   PImage alexRight2;
 
-  // Alex coordinates
-  int intAlexX = 200;
-  int intAlexY = 200;
+  // Alex's coordinates
+  int intAlexX;
+  int intAlexY;
 
-  // Speed
-  int intAlexSpeed = 4;
+  // Alex's Speed
+  int intAlexSpeed = 6;
 
   // Boolean values for Alex's movement
   boolean wPressed = false;
@@ -39,40 +52,124 @@ public class Sketch extends PApplet {
   boolean aPressed = false;
   boolean dPressed = false;
 
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  // Set the size of the window
   public void settings() {
-	// put your size call here
     size(800, 800);
+
+    // Sets Alex's initial position
+    intAlexX = width / 2;
+    intAlexY = height / 2;
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Load and resize all images
   public void setup() {
     
-    // Load the start screen and controls
-    startScreen = loadImage("Start Screen No Buttons.png");
-    controlScreen = loadImage("Game Controls.png")
+    // Load the start screen and resize
+    startScreen = loadImage("Start Screen.png");
+    startScreen.resize(startScreen.width * width/918, startScreen.height * height/918);
+    
+    // Load the buttons on the start screen and resize
+    playButton = loadImage("Play Button.png");
+    playButton.resize(playButton.width * width/918, playButton.height * width/918);
+
+    controlsButton = loadImage("Controls Button.png");
+    controlsButton.resize(controlsButton.width * width/918, controlsButton.height * height/918);
+
+    exitButton = loadImage("Exit Button.png");
+    exitButton.resize(exitButton.width * width/918, exitButton.height * height/918);
+
+    // Load the control screen and resize
+    controlScreen = loadImage("Game Controls.png");
+    controlScreen.resize(controlScreen.width * width/918, controlScreen.height * height/918);
     
     // Load Alex's movement animations
     alexBack1 = loadImage("Alex Back 1.png");
+    alexBack1.resize(alexBack1.width * width/intSizeProportion, alexBack1.height * height/intSizeProportion);
     alexBack2 = loadImage("Alex Back 2.png");
+    alexBack2.resize(alexBack2.width * width/intSizeProportion, alexBack2.height * height/intSizeProportion);
+
     alexForward1 = loadImage("Alex Forward 1.png");
+    alexForward1.resize(alexForward1.width * width/intSizeProportion, alexForward1.height * height/intSizeProportion);
     alexForward2 = loadImage("Alex Forward 2.png");
+    alexForward2.resize(alexForward2.width * width/intSizeProportion, alexForward2.height * height/intSizeProportion);
+    
     alexLeft1 = loadImage("Alex Left 1.png");
+    alexLeft1.resize(alexLeft1.width * width/intSizeProportion, alexLeft1.height * height/intSizeProportion);
     alexLeft2 = loadImage("Alex Left 2.png");
+    alexLeft2.resize(alexLeft2.width * width/intSizeProportion, alexLeft2.height * height/intSizeProportion);
+
     alexRight1 = loadImage("Alex Right 1.png");
+    alexRight1.resize(alexRight1.width * width/intSizeProportion, alexRight1.height * height/intSizeProportion);
     alexRight2 = loadImage("Alex Right 2.png");
+    alexRight2.resize(alexRight2.width * width/intSizeProportion, alexRight2.height * height/intSizeProportion);
+
+    // Load the flashlight
+    flashlight = loadImage("Flashlight.png");
 
   }
 
   // Everything drawn to the screen
   public void draw() {
-	  background(0, 0, 0);
-    // Alex's movement
+    // Draw start screen
+    if (intDraw == -1) {
+      drawStartScreen();
+    }
+    // Draw the controls screen
+    else if (intDraw == 0) {
+      drawControlScreen();
+    }
+    // Draw room 1
+    else if (intDraw == 1) {
+      drawRoom1();
+      drawAlex();
+    }
+    // Draw room 2
+    else if (intDraw == 2) {
+      drawRoom2();
+      drawAlex();
+    }
+    // Draw room 3
+    else if (intDraw == 3) {
+      drawRoom3();
+      drawAlex();
+    }
+  }
+  
+  // Draws the start screen when intDraw = -1
+  public void drawStartScreen() {
+    
+    // Start screen background
+    image(startScreen, 0, 0);
+
+    // Buttons on the start screen
+    image(playButton, width * 100/918, height * 325/918);
+    image(controlsButton, width * 100/918, height * 440/918);
+    image(exitButton, width * 100/918, height * 555/918);
+
+  }
+  
+  // Draws the controls when intDraw = 0
+  public void drawControlScreen() {
+    image(controlScreen, 0, 0);
+  }
+
+  // Draws the first room when intDraw = 1
+  public void drawRoom1() {
+
+  }
+
+  // Draws the second room when intDraw = 2
+  public void drawRoom2() {
+
+  }
+
+  // Draws the third room when intDraw = 3
+  public void drawRoom3() {
+
+  }
+
+  // Draws Alex to the screen
+  public void drawAlex() {
     if (keyPressed) {
       
       if (wPressed) {
@@ -93,7 +190,6 @@ public class Sketch extends PApplet {
       }
     }
   }
-  
   // Alex's movement
   public void keyPressed() {
     if (key == 'w') {
@@ -126,28 +222,33 @@ public class Sketch extends PApplet {
     }
   }
 
-  // Draws the start screen when intArea = -1
-  public void drawStartScreen() {
-
-  }
-  
-  // Draws the controls when intArea = 0
-  public void drawControlScreen() {
-
-  }
-
-  // Draws the first room when intArea = 1
-  public void drawRoom1() {
-
-  }
-
-  // Draws the second room when intArea = 2
-  public void drawRoom2() {
-
-  }
-
-  // Draws the third room when intArea = 3
-  public void drawRoom3() {
-
+  // Checks when the mouse is pressed
+  public void mousePressed() {
+    
+    // Checks mouse interactivity on the start screen
+    if (intDraw == -1) {
+      
+      // Starts the game when the start button is pressed
+      if (mouseX > width * 100/918 && mouseX < width * 100/918 + playButton.width && mouseY > height * 325/918 && mouseY < height * 325/918 + playButton.height) {
+        intDraw = 1;
+      }
+      
+      // Shows the players the control screen when the controls button is pressed
+      else if (mouseX > width * 100/918 && mouseX < width * 100/918 + controlsButton.width && mouseY > height * 440/918 && mouseY < height * 440/918 + controlsButton.height) {
+        intDraw = 0;
+      }
+      // Exits the program when the exit button is pressed
+      else if (mouseX > width * 100/918 && mouseX < width * 100/918 + exitButton.width && mouseY > height * 555/918 && mouseY < height * 555/918 + exitButton.height) {
+        exit();
+      }
+    }
+    
+    // Checks if the player presses the back button on the controls screen
+    else if (intDraw == 0) {
+      
+      if (mouseX > width * 752/918 && mouseX < width * 896/918 && mouseY > height * 808/918 && mouseY < height * 901/918) {
+        intDraw = -1;
+      }
+    }
   }
 }
